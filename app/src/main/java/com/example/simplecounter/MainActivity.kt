b73c4e678dce23e1befda90d1006ec4596b1160b
paperTigerToo <zacharykochemail@gmail.com>
2026-01-29 14:04:43 -0500

    second button attempt

--- app/src/main/java/com/example/simplecounter/MainActivity.kt
+++ app/src/main/java/com/example/simplecounter/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.simplecounter
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -16,13 +17,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val button = findViewById<Button>(R.id.button)
         val txtView = findViewById<TextView>(R.id.textView)
-
+        val buttonUpgrade = findViewById<Button>(R.id.button2)
         var count = 0
+        var incrementBy =1
         txtView.text = count.toString()
         button.setOnClickListener {
             Toast.makeText(it.context, "Clicked Button!", Toast.LENGTH_SHORT).show()
-            count +=1
+            count += incrementBy
             txtView.text = count.toString()
+        }
+
+        if(count>=10) {
+            buttonUpgrade.visibility = View.VISIBLE
+
+            buttonUpgrade.setOnClickListener {
+                incrementBy = 2
+                button.text = "ADD ${incrementBy.toString()}"
+                buttonUpgrade.visibility = View.INVISIBLE
+            }
         }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
